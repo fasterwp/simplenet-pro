7509e210db9dccc20a99f58a1b4a04194752d57e
Andrei Chira <andreichira777@gmail.com>
2017-07-20 23:34:46 +0300

    add prism

--- functions.php
+++ functions.php
@@ -203,3 +203,31 @@ genesis_register_widget_area(
 
 // Enable shortcodes in text widgets
 add_filter('widget_text','do_shortcode');
+
+// Function to add prism.css and prism.js to the site
+function add_prism() {
+// Register prism.css file
+wp_register_style(
+'prismCSS', // handle name for the style so we can register, de-register, etc.
+get_stylesheet_directory_uri() . '/prism.css' // location of the prism.css file
+);
+// Register prism.js file
+wp_register_script(
+'prismJS', // handle name for the script so we can register, de-register, etc.
+get_stylesheet_directory_uri() . '/js/prism.js' // location of the prism.js file
+);
+// Enqueue the registered style and script files
+wp_enqueue_style('prismCSS');
+wp_enqueue_script('prismJS');
+}
+add_action('wp_enqueue_scripts', 'add_prism');
+
+// Remove secondary sidebar.
+unregister_sidebar( 'sidebar-alt' );
+
+// Remove site layouts.
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+
