7f1d2b74d9d34c52966d6e56bcb6680290eb3cbf
Andrei Chira <andreichira777@gmail.com>
2017-08-06 23:15:45 +0300

    icon shortcodes

--- functions.php
+++ functions.php
@@ -233,4 +233,30 @@ genesis_unregister_layout( 'content-sidebar-sidebar' );
 genesis_unregister_layout( 'sidebar-content-sidebar' );
 genesis_unregister_layout( 'sidebar-sidebar-content' );
 
-
+// Font Awesome Shortcodes
+function addscFontAwesome($atts) {
+    extract(shortcode_atts(array(
+    'type'  => '',
+    'size' => '',
+    'rotate' => '',
+    'flip' => '',
+    'pull' => '',
+    'animated' => '',
+    'class' => '',
+  
+    ), $atts));
+ 
+    $classes  = ($type) ? 'fa-'.$type. '' : 'fa-star';
+    $classes .= ($size) ? ' fa-'.$size.'' : '';
+    $classes .= ($rotate) ? ' fa-rotate-'.$rotate.'' : '';
+    $classes .= ($flip) ? ' fa-flip-'.$flip.'' : '';
+    $classes .= ($pull) ? ' pull-'.$pull.'' : '';
+    $classes .= ($animated) ? ' fa-spin' : '';
+    $classes .= ($class) ? ' '.$class : '';
+ 
+    $theAwesomeFont = '<i class="fa '.esc_html($classes).'"></i>';
+      
+    return $theAwesomeFont;
+}
+  
+add_shortcode('icon', 'addscFontAwesome');
